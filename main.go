4e99fb347b69547eccb7fbc054a6fffc3385f907
Ali-Full-Stack <ali230897m@gmail.com>
2024-04-16 09:26:05 +0500

    added text file

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"os"
 
 	"github.com/Ali-Full-stack/Golang-Bootcamp/file"
 )
 
 func main() {
-	s := "11 The quick brown fox jumped over the lazy dog"
-	sr := strings.NewReader(s)
-	counts, err := file.CountLetters(sr)
+	f, err := os.Open("file/text.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	// s := "11 The quick brown fox jumped over the lazy dog"
+	// sr := strings.NewReader(s)
+	counts, err := file.CountLetters(f)
 	if err != nil {
 		fmt.Println(err)
 	}
