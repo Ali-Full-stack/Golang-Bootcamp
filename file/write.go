934f2b9d0d4d598c8a67d7d535c114733e64a36e
Ali-Full-Stack <ali230897m@gmail.com>
2024-04-17 08:56:39 +0500

    new file added

---
+++ file/write.go
@@ -0,0 +1,33 @@
+package file
+
+import (
+	"fmt"
+)
+	// Create a file called user.txt
+	file ,err := os.Create("user.txt")
+	if err != nil {
+		fmt.Println("error creating file :",err)
+		return
+	}
+	defer file.Close()
+
+	var userinput string 
+	// take a user input 
+
+	fmt.Print("Enter a word >>> ")
+	fmt.Scan(&userinput)
+	// write user input to the  file 
+	n,err := file.WriteString(userinput)
+	if err != nil {
+		fmt.Println("error writing to file :",err)
+		return
+	}
+	// read a file
+	fi, err :=os.ReadFile(file)
+	if err != nil {
+		fmt.Println("error reading file :",err)
+		return
+	}
+
+	// display the content of  file to the terminal
+	fmt.Println("file content :",fi)
