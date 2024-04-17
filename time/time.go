9b1ca610f404d4d887ceab0a990aa604bdfc4dac
Ali-Full-Stack <ali230897m@gmail.com>
2024-04-17 10:28:01 +0500

    file changes

--- time/time.go
+++ time/time.go
@@ -6,3 +6,14 @@ fmt.Println(now)
 
 // Unix time 1970 year january 1  till now
 fmt.Println(now.Unix)
+
+// display time in different mode
+fmt.Println(now.Day())
+fmt.Println(now.Month())
+fmt.Println(now.Year())
+fmt.Println(now.Hour())
+fmt.Println(now.Minute())
+fmt.Println(now.Second())
+
+// to sleep the program for 2 second
+time.Sleep(2 * time.Second)
